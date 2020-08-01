b8ac49037dc4b23104ba0b8f72bc02e136f3367c
yusuke-home <yuusukerego@yahoo.co.jp>
2020-08-02 01:17:19 +0900

    fix cors error

--- server/src/record.go
+++ server/src/record.go
@@ -15,6 +15,18 @@ var jst = time.FixedZone("Asia/Tokyo", 9*60*60)
 func Start(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 
+	// Set CORS headers for the preflight request
+	if r.Method == http.MethodOptions {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.Header().Set("Access-Control-Max-Age", "3600")
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
+	// Set CORS headers for the main request.
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+
 	record := &models.Record{
 		Start: time.Now().In(jst),
 	}
@@ -31,6 +43,18 @@ func Start(w http.ResponseWriter, r *http.Request) {
 
 func End(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
+
+	// Set CORS headers for the preflight request
+	if r.Method == http.MethodOptions {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.Header().Set("Access-Control-Max-Age", "3600")
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
+	// Set CORS headers for the main request.
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
