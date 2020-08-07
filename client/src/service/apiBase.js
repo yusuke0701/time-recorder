82e585223df0b7bd84a5072d243cf4568f8ee89d
yusuke-home <yuusukerego@yahoo.co.jp>
2020-08-08 03:30:32 +0900

    set token

--- client/src/service/apiBase.js
+++ client/src/service/apiBase.js
@@ -2,22 +2,46 @@ import axios from 'axios';
 
 export { doGet, doPost, doPut, doDelete };
 
-const axiosInstance = axios.create({
-  baseURL: 'https://us-central1-hoge-hoge-123456789.cloudfunctions.net',
-});
+let axiosInstance;
 
-function doGet(url) {
+async function makeAxiosInstance() {
+  const token = await getLocalStorage('token');
+
+  axiosInstance = axios.create({
+    baseURL: 'https://us-central1-hoge-hoge-123456789.cloudfunctions.net',
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+}
+
+function getLocalStorage(key = null) {
+  return new Promise(resolve => {
+    chrome.storage.local.get(key, item => {
+      key ? resolve(item[key]) : resolve(item);
+    });
+  });
+}
+
+async function doGet(url) {
+  if (axiosInstance) {
+    return axiosInstance.get(url);
+  }
+  await makeAxiosInstance();
   return axiosInstance.get(url);
 }
 
 function doPost(url, data) {
+  // MEMO: doGetが先に呼ばれるので、axiosInstance のチェックはしない
   return axiosInstance.post(url, data);
 }
 
 function doPut(url, data) {
+  // MEMO: doGetが先に呼ばれるので、axiosInstance のチェックはしない
   return axiosInstance.put(url, data);
 }
 
 function doDelete(url, config) {
+  // MEMO: doGetが先に呼ばれるので、axiosInstance のチェックはしない
   return axiosInstance.delete(url, config);
 }
